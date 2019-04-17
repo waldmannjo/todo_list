22613e88e51ab6addde40c029728d7f5daaab1ab
Joachim Waldmann <waldmann.jo@gmail.com>
2019-04-17 13:41:35 +0200

    add remove button. removes all crossed out items

--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,32 @@
-const addTodoButton = document.querySelector('button.addTodoButton');
+const body = document.querySelector('body');
+//const addTodoButton = document.querySelector('button.addTodoButton');
 const addTodoInput = document.querySelector('input.addTodoInput');
 const ul = document.querySelector("ul.todoList");
-//Add new item to Todo List
-addTodoButton.addEventListener('click', () => {
-  if (addTodoInput.value !== "") {
+const lis = ul.children;
+
+body.addEventListener('click', (e) => {
+  //add new item to Todo List
+  if (e.target.className == "addTodoButton" && addTodoInput.value !== "") {
     let li = document.createElement('li');
     li.innerHTML = `<input type='checkbox'> ${addTodoInput.value}`;
     ul.appendChild(li);
     //clear input
     addTodoInput.value = '';
   }
+
+  //remove crossed out items from list
+  if (e.target.id == "removeButton") {
+    if (lis.length > 0) {
+      for (let i = lis.length; i>0; i--) {
+        if (lis[i-1].style.textDecoration == "line-through") {
+          ul.removeChild(lis[i-1]);
+        }
+      }
+    }
+  }
 });
 
+//cross out checked items
 ul.addEventListener('change', (e) => {
   let checkbox = e.target;
   let li = checkbox.parentNode;
